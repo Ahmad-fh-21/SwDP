c4ec7b9ebf59485f27a6bebdc37242cbb2e5d29f
Ahmad <io23m021@technikum-wien.at>
2024-11-25 18:18:50 +0100

    add MQTT cfg and confige P&C

---
+++ Docker_workspace/client/test.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import paho.mqtt.client as mqtt
+import time
+
+client = mqtt.Client()
+
+def on_log(client, userdata, level, buf):
+  print("log: ",buf)
+
+client.on_log=on_log
+
+client.tls_set('/client_certs/ca.crt','/client_certs/client.crt', '/client_certs/client.key')
+
+client.connect('iotgw.local', 8883, 60)
+client.loop_start()
+
+value = 0
+while True:
+    client.publish("/value", value)
+    value += 1
+    time.sleep(1)
+
